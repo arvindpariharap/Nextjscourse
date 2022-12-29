9abe60c6366e543a239b8b7a06d67080f1c8f01e
arvindpariharap <aparihar790@gmail.com>
2022-12-29 18:49:44 +0530

    this is my first comment adding tailwind css

---
+++ codeswear/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CodesWear.com - Wear the code</title>
+        <meta name="description" content="CodesWear.com - Wear the code" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Hey this is CodesWear
+      <div className="mx-4 bg-slate-500">this is me</div>
+    </>
+  )
+}
--- huntingcoder/pages/blogpost/[slug].tsx
+++ huntingcoder/pages/blogpost/[slug].tsx
@@ -19,19 +19,19 @@ const Slug = (props: { myBlog: any; }) => {
 };
 
 export async function getStaticPaths() {
-let allb = await fs.promises.readdir(`blogdata`)
-allb = allb.map((item) =>{
-return { params: {slug: item.split(".")[0]}}
-console.log(allb)
-})
+// let allb = await fs.promises.readdir(`blogdata`)
+// allb = allb.map((item) =>{
+// return { params: {slug: item.split(".")[0]}}
+// console.log(allb)
+// })
   return {
-    paths: allb,
-    // [
-    //   { params: {slug: "how-to-learn-flask"} },
-    //   { params: {slug: "how-to-learn-javascript"} },
-    //   { params: {slug: "how-to-learn-nextjs"} },
+    paths: //allb,
+    [
+      { params: {slug: "how-to-learn-flask"} },
+      { params: {slug: "how-to-learn-javascript"} },
+      { params: {slug: "how-to-learn-nextjs"} },
       
-    // ],
+    ],
     fallback: false, // can also be true or 'blocking'
   }
 }
